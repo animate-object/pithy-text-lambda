49a76f14d74e2004bee42532d29f3176f7461dae
alex prinsen <alexander.f.prinsen@gmail.com>
2018-03-24 14:11:18 -0500

    more test stuff

--- do_test.sh
+++ do_test.sh
@@ -8,8 +8,10 @@ pip install -Ur requirements.txt
 pip install -Ur dev-requirements.txt
 
 echo "Running tests"
+
 pytest -v test/
 
 echo "Tearing down test environment"
+
 deactivate
 rm -rf testenv
--- test/test_lambda_handler.py
+++ test/test_lambda_handler.py
@@ -1,3 +1,6 @@
+from mock import Mock
+from src.lambda_function import lambda_handler, get_random_text
+
 def test_test():
     pass
 
