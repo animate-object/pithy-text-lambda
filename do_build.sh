e7eeb199333d7a14c4f3984d85dd7f8f0fbb73c9
alex prinsen <alexander.f.prinsen@gmail.com>
2018-03-24 16:20:41 -0500

    move testing into build script

--- do_build.sh
+++ do_build.sh
@@ -1,18 +1,46 @@
 #!/usr/bin/env bash
+hr() {
+        printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+}
+hr
+echo Running build
+echo 0 -- run tests
+echo 1 -- install dependencies
+echo 2 -- bundle code and dependencies
+echo 3 -- zip it up
+hr
 
 set -e
 
-rm -rf build.zip
-mkdir build
+./do_test.sh
 
+rm -rf build.zip
+
+hr
+echo Installing dependencies
+hr
+
+mkdir build
 virtualenv venv --python=$(which python3.6)
 source venv/bin/activate
 pip install -Ur requirements.txt
 deactivate
 
+hr
+echo Bundling code
+hr
+
 cp -r src/*.py build/
 cp -r venv/lib/python3.6/site-packages/* build/
 
-zip -r build.zip build
+hr
+echo Compressing
+hr
 
+zip -r build.zip build
 rm -rf venv build
+
+hr
+echo Build complete -- check build.zip
+hr
+
--- do_test.sh
+++ do_test.sh
@@ -1,17 +1,32 @@
+#!/usr/bin/env bash
+
+hr() {
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+}
+
+cleanup() {
+    exit_code=$?
+    hr
+    echo Tearing down test env
+    hr
+    deactivate
+    rm -rf testenv
+    [ $exit_code -eq 0 ] & echo Test run successful & hr
+    exit $exit_code
+}
+
+trap 'cleanup' INT TERM EXIT
+
 set -e
-
+hr
 echo "Setting up test environment"
-
+hr
 virtualenv testenv --python=$(which python3.6)
 source testenv/bin/activate
 pip install -Ur requirements.txt
 pip install -Ur dev-requirements.txt
-
+hr
 echo "Running tests"
-
+hr
 pytest -vv test/
 
-echo "Tearing down test environment"
-
-deactivate
-rm -rf testenv
